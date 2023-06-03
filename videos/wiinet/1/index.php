94906c6d427fe4aa4492ee9d4f3f6eb8c678b701
soafen <soafensoe@protonmail.com>
2023-06-03 23:25:18 +0200

    June 3rd updates
    -remove WiiNet YouTube
    + update marquee
    + add WiiNet channel to videos
    + update latest videos
    + add creator name to videos list

--- videos/wiinet/1/index.php
+++ videos/wiinet/1/index.php
@@ -7,13 +7,12 @@
             <object type="application/x-shockwave-flash" data="../../resources/player.swf" width="384" height="256">
                 <param name="wmode" value="transparent">
                 <param name="allowFullScreen" value="false">
-                <param name='flashvars' value='filename=sophiegaming-1-1.flv'>
+                <param name='flashvars' value='filename=wiinet-1.flv'>
             </object>
 			</div>
-            PART 1 - <a href="index2.html">PART 2</a>
-            <h1>starting 2023 by playing TF2 on potato PCs</h1>
+            <h1>WiiNet is available now!</h1>
             <div style="font-size:20px;"><?php class pageView { private $file = 'views.txt'; public function add_view() { $prev_count = file_get_contents($this->file); file_put_contents($this->file, $prev_count+1); } public function get_view() { $count = file_get_contents($this->file); return $count; } } $view = new pageView; $view->add_view(); echo $view->get_view().' views'; ?> - 
-			Jan 2, 2023
+			Feb 7, 2023
 			</div>
         </div>
         <table>
@@ -22,11 +21,11 @@
                     <a href="../"><img src="../pfp.jpg" style="max-width:100px;"></a>
                 </td>
                 <td>
-                    <a href="../">sophieGaming</a>
+                    <a href="../">WiiNet</a>
 				</td>
 				<td style="padding-left:20px;">
 					<h2>Description</h2>
-					how else should we spend our first day of the new year than acting like it's 2012 with 2 old Windows 7 PCs and TF2 on mastercomfig's lowest offerings..?
+					Try out WiiNet at wiinet.xyz
 				</td>
             </tr>
             </table>
--- videos/wiinet/2/index.php
+++ videos/wiinet/2/index.php
@@ -7,13 +7,13 @@
             <object type="application/x-shockwave-flash" data="../../resources/player.swf" width="384" height="256">
                 <param name="wmode" value="transparent">
                 <param name="allowFullScreen" value="false">
-                <param name='flashvars' value='filename=sophiegaming-1-1.flv'>
+                <param name='flashvars' value='filename=wiinet-2-1.flv'>
             </object>
 			</div>
             PART 1 - <a href="index2.html">PART 2</a>
-            <h1>starting 2023 by playing TF2 on potato PCs</h1>
+            <h1>What's next for WiiNet? - Project update (June 2023)</h1>
             <div style="font-size:20px;"><?php class pageView { private $file = 'views.txt'; public function add_view() { $prev_count = file_get_contents($this->file); file_put_contents($this->file, $prev_count+1); } public function get_view() { $count = file_get_contents($this->file); return $count; } } $view = new pageView; $view->add_view(); echo $view->get_view().' views'; ?> - 
-			Jan 2, 2023
+			June 3, 2023
 			</div>
         </div>
         <table>
@@ -22,11 +22,11 @@
                     <a href="../"><img src="../pfp.jpg" style="max-width:100px;"></a>
                 </td>
                 <td>
-                    <a href="../">sophieGaming</a>
+                    <a href="../">WiiNet</a>
 				</td>
 				<td style="padding-left:20px;">
 					<h2>Description</h2>
-					how else should we spend our first day of the new year than acting like it's 2012 with 2 old Windows 7 PCs and TF2 on mastercomfig's lowest offerings..?
+					soafen and Turtle discuss the current progress with WiiNet and future plans for the project, along with how you can help out!
 				</td>
             </tr>
             </table>
